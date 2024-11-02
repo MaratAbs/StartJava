c27f94a098d752e3bd37f77e8c41c7e8d99885f5
MaratAbs <absatdarov43@gmail.com>
2024-11-02 13:22:03 +0700

    внесены изменения во всех заданиях

--- Lesson1/IfElseStatementTheme.java
+++ Lesson1/IfElseStatementTheme.java
@@ -16,16 +16,13 @@ public class IfElseStatementTheme {
             System.out.println("Гражданин считается несовершеннолетним.");
         }
 
-        double growthOfTheRecruit = 1.75;
-        if (growthOfTheRecruit < 1.80) {
+        double heightOfTheRecruit = 1.75;
+        if (heightOfTheRecruit < 1.80) {
             System.out.println("Призывник может служить в танковых войсках.");
         } else {
             System.out.println("Призывник может служить в иных войсках.");
         }
-
-        String name1 = "Max";
-        String name2 = "Ilona";
-        String name3 = "Alex";
+        
         char firstLetterName = "Max".charAt(0);
         if (firstLetterName == 'M') {
             System.out.println("Третья группа практикантов.");
@@ -49,56 +46,49 @@ public class IfElseStatementTheme {
 
         // ЗАДАНИЕ 3
         System.out.println("\n3. Проверка числа\n");
-        int number = -4;
-        if (number == 0) {
-            System.out.println("Переменная инициализирована нулём.");
-        } else if (number % 2 == 0) {
-            if (number < 0) {
-                System.out.println(number + " является отрицательным и чётным");
+        int num = -4;
+        if (num != 0) {
+            if (num >= 1) {
+                System.out.print(num + " - положительное");
             } else {
-                System.out.println(number + " является положительным и чётным");
+                System.out.print(num + " - отрицательное");
             }
-        } else if (number > 0) {
-            System.out.println(number + " является положительным и нечётным");
+            if (num % 2 == 0) {
+                System.out.println(" и чётное");
+            } else {
+                System.out.println(" и нечётное");
+            }
         } else {
-            System.out.println(number + " является отрицательным и нечётным");
+            System.out.println("Переменная инициализирована нулём.");
         }
 
         // ЗАДАНИЕ 4
         System.out.println("\n4. Поиск одинаковых цифр в числах\n");
-        int firstNum = 123;
-        int secondNum = 223;
-        int firstDigitOfFirstNum = firstNum / 100;
-        int secondDigitOfFirstNum = firstNum % 100 / 10;
-        int thirdDigitOfFirstNum = firstNum % 10;
-        int firstDigitOfSecondNum = secondNum / 100;
-        int secondDigitOfSecondNum = secondNum % 100 / 10;
-        int thirdDigitOfSecondNum = secondNum % 10;
-        boolean isEqualNumbers = ((firstDigitOfFirstNum == firstDigitOfSecondNum) ||
-                (secondDigitOfFirstNum == secondDigitOfSecondNum) ||
-                (thirdDigitOfFirstNum == thirdDigitOfSecondNum));
+        num1 = 123;
+        num2 = 223;
+        int hudredsNum1 = num1 / 100;
+        int tensNum1 = num1 % 100 / 10;
+        int onesNum1 = num1 % 10;
+        int hundredsNum2 = num2 / 100;
+        int tensNum2 = num2 % 100 / 10;
+        int onesNum2 = num2 % 10;
+        boolean isEqualNumbers = ((hudredsNum1 == hundredsNum2) ||
+                (tensNum1 == tensNum2) || (onesNum1 == onesNum2));
         if (isEqualNumbers) {
-            System.out.println("Есть совпадения цифр в разрядах чисел.");
-            if (firstNum == secondNum) {
-                System.out.println(firstNum + " == " + secondNum);
-            } else if (firstNum < secondNum) {
-                System.out.println(firstNum + " < " + secondNum);
-            } else {
-                System.out.println(firstNum + " > " + secondNum);
-            }
+            System.out.println("Исходные числа: " + num1 + " и " + num2);
             System.out.println("Одинаковые цифры в разрядах чисел: ");
-            if (thirdDigitOfFirstNum == thirdDigitOfSecondNum) {
-                System.out.println(thirdDigitOfFirstNum + " - разряд единиц");
+            if (onesNum1 == onesNum2) {
+                System.out.println(onesNum1 + " - разряд единиц");
             }
-            if (secondDigitOfFirstNum == secondDigitOfSecondNum) {
-                System.out.println(secondDigitOfFirstNum + " - разряд десятков");
+            if (tensNum1 == tensNum2) {
+                System.out.println(tensNum1 + " - разряд десятков");
             }
-            if (firstDigitOfFirstNum == firstDigitOfSecondNum) {
-                System.out.println(firstDigitOfFirstNum + " - разряд сотен");
+            if (hudredsNum1 == hundredsNum2) {
+                System.out.println(hudredsNum1 + " - разряд сотен");
             }
         } else {
-            System.out.println("Ни одна цифра в разрядах чисел " + firstNum + " и " + secondNum +
-                    " не совпадает");
+            System.out.println("Ни одна цифра в разрядах чисел " + 
+                    num1 + " и " + num2 + " не совпадает");
         }
 
         // ЗАДАНИЕ 5
@@ -117,23 +107,16 @@ public class IfElseStatementTheme {
         // ЗАДАНИЕ 6
         System.out.println("\n6. Подсчет начисленных банком %\n");
         double amountOfDeposite = 321123.59;
-        double amountOfInterest = 0;
-        double interestRate = 0;
-        int interestPeriod = 365;
-        int numberOfCalenderDays = 365;
+        double interestRate = 10;
         if (amountOfDeposite < 100000) {
             interestRate = 5;
-            amountOfInterest = (amountOfDeposite * interestRate * interestPeriod / 
-                numberOfCalenderDays) / 100;
         } else if (amountOfDeposite >= 100000 && amountOfDeposite <= 300000) {
             interestRate = 7;
-            amountOfInterest = (amountOfDeposite * interestRate * interestPeriod / 
-                numberOfCalenderDays) / 100;
-        } else {
-            interestRate = 10;
-            amountOfInterest = (amountOfDeposite * interestRate * interestPeriod / 
-                numberOfCalenderDays) / 100;
         }
+        int interestPeriod = 365;
+        int numberOfCalenderDays = 365;
+        double amountOfInterest = (amountOfDeposite * interestRate * interestPeriod / 
+                numberOfCalenderDays) / 100;
         System.out.printf("Сумма вклада: %.2f руб.\n" + 
                 "Сумма начисленных процентов за год: %.2f руб.\n" +
                 "Сумма вклада в конце срока: %.2f руб.\n", 
@@ -143,46 +126,38 @@ public class IfElseStatementTheme {
         System.out.println("\n7. Определение оценки по предметам\n");
         double historyPercentScore = 59;
         double csPercentScore = 92;
-        int historyGrade = 0;
-        int csGrade = 0;
+        int historyGrade = 5;
         if (historyPercentScore <= 60) {
             historyGrade = 2;
         } else if (historyPercentScore > 60 && historyPercentScore <= 73) {
             historyGrade = 3;
         } else if (historyPercentScore > 73 && historyPercentScore <= 91) {
             historyGrade = 4;
-        } else {
-            historyGrade = 5;
-        }
+        } 
+        int csGrade = 5;
         if (csPercentScore <= 60) {
             csGrade = 2;
         } else if (csPercentScore > 60 && csPercentScore <= 73) {
             csGrade = 3;
         } else if (csPercentScore > 73 && csPercentScore <= 91) {
             csGrade = 4;
-        } else {
-            csGrade = 5;
         }
         System.out.println("История - " + historyGrade);
         System.out.println("Программирование - " + csGrade);
         System.out.println("Средний балл оценок по предмету: " + (historyGrade + csGrade) / 2F);
         System.out.println("Средний процент баллов: " + 
-                      (historyPercentScore + csPercentScore) / 2F);
+                (historyPercentScore + csPercentScore) / 2F);
 
         // ЗАДАНИЕ 8
         System.out.println("\n8. Расчет годовой прибыли\n");
         double monthlySales = 13025.233;
         double monthlyRent = 5123.018;
         double monthlyProductionCosts = 9001.729;
-        int numberOfMonths = 12;
-        double annualProfit = (monthlySales - monthlyRent - monthlyProductionCosts) * 
-                numberOfMonths;
-        if (annualProfit < 0) {
+        double annualProfit = (monthlySales - monthlyRent - monthlyProductionCosts) * 12;
+        if (annualProfit < 0 || annualProfit == 0) {
             System.out.printf("Прибыль за год: %.2f%n", annualProfit);
         } else if (annualProfit > 0) {
             System.out.printf("Прибыль за год: %+.2f%n", annualProfit);
-        } else {
-            System.out.printf("Прибыль за год: %.2f%n", annualProfit);
         }
     }
 }
