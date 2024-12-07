2ebcab4b186ff70e8ae6a1e52550d43040c14fc8
MaratAbs <absatdarov43@gmail.com>
2024-12-07 12:06:27 +0700

    объединение строк, вынесение инструкции за пределы цикла

--- Lesson1/Game.java
+++ Lesson1/Game.java
@@ -2,18 +2,18 @@ import java.util.Scanner;
 
 public class Game {
     public static void main(String[] args) {
-        int randomNumber = 1 + (int) (Math.random() * 99) + 1;
+        int randomNumber = 1 + (int) (Math.random() * 100);
         System.out.print("Введите число в диапазоне от 1 до 100: ");
-        while (true) {
-            int playerNumber = new Scanner(System.in).nextInt();
-            if (playerNumber == randomNumber) {
-                System.out.println("Вы угадали!");
-                break;
-            } else if (playerNumber > randomNumber) {
+        Scanner console = new Scanner(System.in);
+        int playerNumber = 0;
+        while (playerNumber != randomNumber) {
+            playerNumber = console.nextInt();
+            if (playerNumber > randomNumber) {
                 System.out.print(playerNumber + " больше загаданного. Введите другое число: ");
-            } else {
+            } else if (playerNumber < randomNumber) {
                 System.out.print(playerNumber + " меньше загаданного. Введите другое число: ");
             }
         }
+        System.out.println("Вы угадали!");
     }
 }
