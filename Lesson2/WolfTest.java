4a44320d1ac8469cb1554d81c91eeacb973865f6
MaratAbs <absatdarov43@gmail.com>
2024-12-11 19:53:33 +0700

    добавил строку после объявл. класса

--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -1,15 +1,16 @@
 public class WolfTest {
+
     public static void main(String[] args) {
         Wolf wolfOne = new Wolf();
-
         wolfOne.gender = "самец";
         wolfOne.name = "Вася";
         wolfOne.weight = 20.6F;
         wolfOne.age = 1;
         wolfOne.color = "белый";
-
-        System.out.println("Пол: " + wolfOne.gender + "\nКличка: " + wolfOne.name + 
-                "\nВес: " + wolfOne.weight + " кг." + "\nВозраст: " + wolfOne.age + " год" +
+        System.out.println("Пол: " + wolfOne.gender +
+                "\nКличка: " + wolfOne.name + 
+                "\nВес: " + wolfOne.weight + " кг." +
+                "\nВозраст: " + wolfOne.age + " г." +
                 "\nОкрас: " + wolfOne.color);
         wolfOne.go();
         wolfOne.sit();
