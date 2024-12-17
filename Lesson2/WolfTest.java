c03444101acd2e1682b67d93454be632cbbb95db
MaratAbs <absatdarov43@gmail.com>
2024-12-17 12:00:46 +0700

    внесены изменения

--- Lesson2/WolfTest.java
+++ Lesson2/WolfTest.java
@@ -5,13 +5,13 @@ public class WolfTest {
         wolfOne.setGender("самец");
         wolfOne.setName("Вася");
         wolfOne.setWeight(20.6F);
-        wolfOne.setAge("10");
+        wolfOne.setAge(10);
         wolfOne.setColor("белый");
-        System.out.println("Пол: " + wolfOne.getGender() +
-                "\nКличка: " + wolfOne.getName() + 
-                "\nВес: " + wolfOne.getWeight() + " кг." +
-                "\nВозраст: " + wolfOne.getAge() +
-                "\nОкрас: " + wolfOne.getColor());
+        System.out.println("Пол: " + wolfOne.getGender());
+        System.out.println("Кличка: " + wolfOne.getName());
+        System.out.println("Вес: " + wolfOne.getWeight());
+        System.out.println("Возраст: " + wolfOne.getAge());
+        System.out.println("Окрас: " + wolfOne.getColor());
         wolfOne.go();
         wolfOne.sit();
         wolfOne.run();
