39c5baf3f40ad73eba08da84c68d21d17b3cd402
MaratAbs <absatdarov43@gmail.com>
2024-12-28 12:14:23 +0700

    применение Guard Clauses, устран. дублирования кода

--- Lesson2/GuessNumber.java
+++ Lesson2/GuessNumber.java
@@ -2,42 +2,53 @@ import java.util.Scanner;
 
 public class GuessNumber {
 
-    Player p1;
-    Player p2;
+    private Player p1;
+    private Player p2;
+    Scanner console = new Scanner(System.in);
+    int playerNumber;
+    int randomNumber;
 
-    GuessNumber(Player p1, Player p2) {
+    public GuessNumber(Player p1, Player p2) {
         this.p1 = p1;
         this.p2 = p2;
     }
 
-    Scanner console = new Scanner(System.in);
-
-    public void startGame() {
+    public void start() {
         System.out.println("Угадай число от 1 до 100.");
-        int randomNumber = (int) (1 + Math.random() * 100);
-        int playerNumber = 0;
+        randomNumber = (int) (1 + Math.random() * 10);
         while (true) {
-            System.out.print(p1.getName() + ", введи предполагаемое число: ");
-            playerNumber = console.nextInt();
-            if (playerNumber < randomNumber) {
-                System.out.println(" Число " + playerNumber + " меньше, чем загаданное.");
-            } else if (playerNumber > randomNumber) {
-                System.out.println(" Число " + playerNumber + " больше, чем загаданное.");
-            } else {
-                System.out.println(" Победил(а) " + p1.getName() + "!");
-                break;
-            }
-
-            System.out.print(p2.getName() + ", введи предполагаемое число: ");
-            playerNumber = console.nextInt();
-            if (playerNumber < randomNumber) {
-                System.out.println(" Число " + playerNumber + " меньше, чем загаданное.");
-            } else if (playerNumber > randomNumber) {
-                System.out.println(" Число " + playerNumber + " больше, чем загаданное.");
-            } else {
-                System.out.println(" Победил(а) " + p2.getName() + "!");
-                break;
-            }
+            compareFirstNumber();
+            if (playerNumber == randomNumber) return;
+            compareSeconsNumber();
+            if (playerNumber == randomNumber) return;
         }
+    }
+
+    public void compareFirstNumber() {
+        System.out.print(p1.getName() + ", введи предполагаемое число: ");
+            playerNumber = console.nextInt();
+            if (playerNumber == randomNumber) {
+                System.out.println(" Победил(а) " + p1.getName() + "!");
+                return;
+            }
+            if (playerNumber > randomNumber) {
+                System.out.println(" Число " + playerNumber + " больше, чем загаданное.");
+            } else {
+                System.out.println(" Число " + playerNumber + " меньше, чем загаданное.");
+            }
+    }
+
+    public void compareSeconsNumber() {
+        System.out.print(p2.getName() + ", введи предполагаемое число: ");
+            playerNumber = console.nextInt();
+            if (playerNumber == randomNumber) {
+                System.out.println(" Победил(а) " + p2.getName() + "!");
+                return;
+            }
+            if (playerNumber > randomNumber) {
+                System.out.println(" Число " + playerNumber + " больше, чем загаданное.");
+            } else {
+                System.out.println(" Число " + playerNumber + " меньше, чем загаданное.");
+            }
     }
 }
