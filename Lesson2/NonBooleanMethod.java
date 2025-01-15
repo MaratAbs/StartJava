1f0650c319a883b0838554f89bbfafed0286bbf6
MaratAbs <absatdarov43@gmail.com>
2025-01-15 10:59:41 +0700

    из класса перенесены вызовы методов в класс Main

--- Lesson2/NonBooleanMethod.java
+++ Lesson2/NonBooleanMethod.java
@@ -1,102 +1,70 @@
 public class NonBooleanMethod {
 
-    public void executeFirstMethodBlock() {
-        findLongestWord();
-        selectMenuItem();
-        countNumberUiqueWords();
-        displayErrorMessage();
-        synchronizeData();
-        restoreData();
-        pauseFileDownload();
-        resetToFactoryDefaults();
-        writeContentsToFile();
-        convertTemperature();
-        enterMathExpression();
-        determineWinner();
-        findBookByAuthor();
-    }
-
-    private void findLongestWord() {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        System.out.println(stackTrace[1].getMethodName() + 
+    public void findLongestWord() {
+        System.out.println(Method.revertName() + 
                 "() - найти самое длинное слово в предложении из книги по Java");
     }
 
-    private void selectMenuItem() {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        System.out.println(stackTrace[1].getMethodName() + 
+    public void selectMenuItem() {
+        System.out.println(Method.revertName() + 
                 "() - выбрать пункт меню в текстовом редакторе на macOS");
     }
 
-    private void calculateAverageGrades() {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        System.out.println(stackTrace[1].getMethodName() + 
+    public void calculateAverageGrades() {
+        System.out.println(Method.revertName() + 
                 "() - вычислить среднее значение оценок в школе №1234");
     }
 
-    private void countNumberUiqueWords() {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        System.out.println(stackTrace[1].getMethodName() + 
+    public void countNumberUiqueWords() {
+        System.out.println(Method.revertName() + 
                 "() - подсчитать количество уникальных слов в \"Война и Мир\"");
     }
 
-    private void displayErrorMessage() {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        System.out.println(stackTrace[1].getMethodName() + 
-                "() - вывести сообщение об ошибке");
+    public void displayErrorMessage() {
+        System.out.println(Method.revertName() + "() - вывести сообщение об ошибке");
     }
 
-    private void synchronizeData() {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        System.out.println(stackTrace[1].getMethodName() + 
+    public void synchronizeData() {
+        System.out.println(Method.revertName() + 
                 "() - синхронизировать данные с облачным хранилищем");
     }
 
-    private void restoreData() {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        System.out.println(stackTrace[1].getMethodName() + 
+    public void restoreData() {
+        System.out.println(Method.revertName() + 
                 "() - восстановить данные из резервной копии от 11.03.2024");
     }
 
-    private void pauseFileDownload() {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        System.out.println(stackTrace[1].getMethodName() + 
+    public void pauseFileDownload() {
+        System.out.println(Method.revertName() + 
                 "() - приостановить загрузку mp3-файла группы \"Ария\"");
     }
 
-    private void resetToFactoryDefaults() {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        System.out.println(stackTrace[1].getMethodName() + 
+    public void resetToFactoryDefaults() {
+        System.out.println(Method.revertName() + 
                 "() - сбросить настройки до заводских для пылесоса Mi");
     }
 
-    private void writeContentsToFile() {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        System.out.println(stackTrace[1].getMethodName() + 
+    public void writeContentsToFile() {
+        System.out.println(Method.revertName() + 
                 "() - записать содержимое в файл по указанному пути на флешку");
     }
     
-    private void convertTemperature() {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        System.out.println(stackTrace[1].getMethodName() + 
+    public void convertTemperature() {
+        System.out.println(Method.revertName() + 
                 "() - преобразовать температуру из Цельсия в Фаренгейт");
     }
 
-    private void enterMathExpression() {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        System.out.println(stackTrace[1].getMethodName() + 
+    public void enterMathExpression() {
+        System.out.println(Method.revertName() + 
                 "() - ввести математическое выражение с тремя аргументами");
     }
 
-    private void determineWinner() {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        System.out.println(stackTrace[1].getMethodName() + 
+    public void determineWinner() {
+        System.out.println(Method.revertName() + 
                 "() - выявить победителя среди гонщиков игры \"Need For Speed\"");
     }
 
-    private void findBookByAuthor() {
-        StackTraceElement[] stackTrace = Thread.currentThread().getStackTrace();
-        System.out.println(stackTrace[1].getMethodName() + 
-                "() - найти книгу по имени писателя");
+    public void findBookByAuthor() {
+        System.out.println(Method.revertName() + "() - найти книгу по имени писателя");
     }
 }
