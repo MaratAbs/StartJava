3bc99c01e5bc694da0da88027ebfa994c86c9bd1
MaratAbs <absatdarov43@gmail.com>
2025-01-15 15:34:03 +0700

    скоррект. имя стат. метода

--- Lesson2/Method.java
+++ Lesson2/Method.java
@@ -1,6 +1,6 @@
 public class Method {
     
-    public static String revertName() {
+    public static String getCurrName() {
         return Thread.currentThread().getStackTrace()[2].getMethodName();
     }
 }
